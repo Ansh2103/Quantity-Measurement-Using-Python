57ce7975e3fe3d8f9194dc72c4c9b78b25bd0143
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-26 03:17:26 +0530

    [SHUBHAM] Added : Functionality to test and validate Weights inQuantity Measurement

--- Main/Quantity_Measurement.py
+++ Main/Quantity_Measurement.py
@@ -1,115 +1,37 @@
-import logging
-
-from Main.QualityMeasurementException import ExceptionQuantityMeasurement
-
-logging.basicConfig(filename='new_measurement_system.log', level=logging.DEBUG,
-                    format=' %(asctime)s| %(name)s |%(message)s  |%(levelname)s ')
+import enum
 
 
-class QuantityMeasurementSystem():
-    GRAMS_TO_KILOGRAM = .001
-    GRAMS_TO_MILLIGRAM = 1000
-    METER_TO_CENTIMETER = 100
-    METER_TO_KILOMETER = 1000
+class QuantityMeasurements:
+    def __init__(self, unit, value):
+        self.__unit = unit
+        self.__value = value
 
-    def temperature_celsius_to_fareheit(self, input_temperature):
-        '''
+    def __eq__(self, other):
+        if isinstance(other, QuantityMeasurements):
+            return self.__value == other.__value
+        return False
 
-        :param input_temperature: input shoud be entered by user
-        :return: this method will convert temperature celcius
-                    into farenheit
-        '''
+    def compare(self, other):
+        if self.__unit.__class__ == other.__unit.__class__:
+            if self.__unit.__class__.convert(self.__unit, self.__value) == other.__unit.__class__.convert(other.__unit,
+                                                                                                          other.__value):
+                return True
+        return False
 
-        try:
-            fahrenheit_temperature = round(((1.8 * input_temperature) + 32), 2)
-            logging.debug("fahrenheit is {}".format(fahrenheit_temperature))
-            return fahrenheit_temperature
-        except TypeError:
-            logging.exception("Exception Occured due to invalid input")
-            raise ExceptionQuantityMeasurement("Provide valid number input")
-
-
-    def temperature_farenheit_to_celsius(self, input_temperature):
-        '''
-
-               :param input_temperature: input shoud be entered by user
-               :return: this method will convert temperature freheit
-                           into celcius
-               '''
-
-        try:
-            celsius_temperature = round(((input_temperature - 32) * 0.56), 3)
-            logging.debug("celsius is {}".format(celsius_temperature))
-            return celsius_temperature
-        except TypeError:
-            logging.exception("Exception Occured due to invalid input")
-            raise ExceptionQuantityMeasurement("Provide valid number input")
+    def add(self, other):
+        if self.__unit.__class__ == other.__unit.__class__:
+            return self.__unit.__class__.convert(self.__unit, self.__value) + other.__unit.__class__.convert(
+                other.__unit, other.__value)
+        return 0
 
 
-    def weight_grams_to_kilograms_conversion(self, input_weight):
-        '''
+class Weights(enum.Enum):
+    kilo_gram = 1.0
+    gram = 0.001
+    tonne = 1000
 
-               :param input_temperature: input shoud be entered by user
-               :return: this method will convert Weight Gram
-                           into kilogran
-               '''
+    def __init__(self, unit):
+        self.unit = unit
 
-        try:
-            kilo_gram_weight = round((input_weight * self.GRAMS_TO_KILOGRAM), 3)
-            logging.debug("kilogram is {}".format(kilo_gram_weight))
-            return kilo_gram_weight
-        except TypeError:
-            logging.exception("Exception Occured due to invalid input")
-            raise ExceptionQuantityMeasurement("Provide valid number input")
-
-
-    def weight_grams_to_milligrams_conversion(self, input_weight):
-        '''
-
-               :param input_temperature: input shoud be entered by user
-               :return: this method will convert weight Gram
-                           into milligram
-               '''
-
-        try:
-            milli_gram_weight = round((input_weight *  self.GRAMS_TO_MILLIGRAM), 3)
-            logging.debug("milliGram is {}".format(milli_gram_weight))
-            return milli_gram_weight
-        except TypeError:
-            logging.exception("Exception Occured due to invalid input")
-            raise ExceptionQuantityMeasurement("Provide valid number input")
-
-
-
-    def length_meter_to_centimeter_conversion(self, input_length):
-        '''
-
-               :param input_temperature: input should be entered by user
-               :return: this method will convert length meter
-                           into centimeter
-               '''
-
-        try:
-            centi_meter_length = round((input_length * self.METER_TO_CENTIMETER), 3)
-            logging.debug("centimeter is {}".format(centi_meter_length))
-            return centi_meter_length
-        except TypeError:
-            logging.exception("Exception Occured due to invalid input")
-            raise ExceptionQuantityMeasurement("Provide valid number input")
-
-
-    def length_meter_to_kilometer_conversion(self, input_length):
-        '''
-
-               :param input_temperature: input shoud be entered by user
-               :return: this method will convert lenght meter
-                           into kilometer
-               '''
-
-        try:
-            kilo_meter_length = round((input_length / self.METER_TO_KILOMETER), 3)
-            logging.debug("kilometer is {}".format(kilo_meter_length))
-            return kilo_meter_length
-        except TypeError:
-            logging.exception("Exception Occured due to invalid input")
-            raise ExceptionQuantityMeasurement("Provide valid number input")
+    def convert(self, value):
+        return self.unit * value
--- Test/test_QuantityMeasurement.py
+++ Test/test_QuantityMeasurement.py
@@ -1,38 +1,22 @@
+from Main.Quantity_Measurement import *
 import pytest
 
-@pytest.mark.parametrize("input, output", [(32, 89.6), (33, 91.4)])
-def test_temperature_celsius_to_farenheit(input, output, instance_of_main_class):
-    result = instance_of_main_class.temperature_celsius_to_fareheit(input)
-    assert result == output
 
 
-@pytest.mark.parametrize("input, output", [(33, 0.56), (337, 170.8)])
-def test_temperature_farenheit_to_celsius(input, output, instance_of_main_class):
-    result = instance_of_main_class.temperature_farenheit_to_celsius(input)
-    assert result == output
+@pytest.mark.parametrize('first_unit, first_value, second_unit,second_value,output',
+                         [(Weights.kilo_gram, 30, Weights.gram, 30000, True), (Weights.tonne, 0.033, Weights.gram,
+                                                                               33000, True)])
+def testing_QuantityMeasurementBy_WeightConversion(first_unit, first_value, second_unit, second_value, output):
+    first_weight = QuantityMeasurements(first_unit, first_value)
+    second_weight = QuantityMeasurements(second_unit, second_value)
+    assert first_weight.compare(second_weight) == output
 
 
-@pytest.mark.parametrize("input, output", [(12, 12 / 1000 ), (7, 7 /1000), (3, 3 / 1000)])
-def test_length_meter_to_kilometer(input, output, instance_of_main_class):
-    result = instance_of_main_class.length_meter_to_kilometer_conversion(input)
-    assert result == output
-
-
-@pytest.mark.parametrize("input, output", [(12, 12 * 100), (7, 7 * 100), (3, 3 * 100)])
-def test_length_meter_to_centimeter(input, output, instance_of_main_class):
-    result = instance_of_main_class.length_meter_to_centimeter_conversion(input)
-    assert result == output
-
-
-
-@pytest.mark.parametrize("input, output", [(3, 3000), (7, 7000),])
-def test_weight_grams_to_milligrams(input, output, instance_of_main_class):
-    result = instance_of_main_class.weight_grams_to_milligrams_conversion(input)
-    assert result == output
-
-@pytest.mark.parametrize("input, output", [(3000, 3), (7000, 7)])
-def test_weight_grams_to_kilograms(input, output, instance_of_main_class):
-    result = instance_of_main_class.weight_grams_to_kilograms_conversion(input)
-    assert result == output
-
+@pytest.mark.parametrize('first_unit, first_value, second_unit,second_value,output',
+                         [(Weights.kilo_gram, 20, Weights.gram, 20000, 40), (Weights.tonne, 20, Weights.kilo_gram,
+                                                                             20000, 40000)])
+def testing_QuantityMeasurementBy_AddingWeights(first_unit, first_value, second_unit,second_value, output):
+    first_weight = QuantityMeasurements(first_unit, first_value)
+    second_weight = QuantityMeasurements(second_unit, second_value)
+    assert first_weight.add(second_weight) == output
 
